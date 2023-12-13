2f89caa03072aeb2850ece7d6bfcb3a12b0c326f
mvboiii <manavbnagda@gmail.com>
2023-12-13 11:38:05 -0500

    Task 2.3.2 #debugging test.cpp
    Debugging the test.cpp for publisher

--- test/publisher_test.cpp
+++ test/publisher_test.cpp
@@ -25,11 +25,11 @@ class TestPublisher : public testing::Test {
  * 
  */
 TEST_F(TestPublisher, test_num_publishers) {
-  node_ = rclcpp::Node::make_shared("test_pub");
+  node_ = rclcpp::Node::make_shared("testpub");
   auto test_pub = node_->create_publisher<std_msgs::msg::String>
-                    ("Publisher active", 5.0);
+                    ("publisher_active", 10.0);
     
-  auto pub = node_->count_publishers("Publisher active");
+  auto pub = node_->count_publishers("publisher_active");
   EXPECT_EQ(1, static_cast<int>(pub));
 }
 /**
@@ -37,12 +37,12 @@ TEST_F(TestPublisher, test_num_publishers) {
  * 
  */
 TEST_F(TestPublisher, test_num_subscribers) {
-  node_ = rclcpp::Node::make_shared("test_sub");
-  auto test_sub = node_->create_subscription<std_msgs::msg::String>("Publisher active", 10.0,
+  node_ = rclcpp::Node::make_shared("testsub");
+  auto test_sub = node_->create_subscription<std_msgs::msg::String>("publisher_active", 10.0,
     [](const std_msgs::msg::String::SharedPtr msg) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Received message: '%s'", msg->data.c_str());
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "I heard: '%s", msg->data.c_str());
     });
 
-  auto sub = node_->count_subscribers("Subscriber active");
+  auto sub = node_->count_subscribers("subscriber_active");
   EXPECT_EQ(0, static_cast<int>(sub));
 }
